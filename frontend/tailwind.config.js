1df0300b1c797e0932112e9d2bf838a5ab0ddad5
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-03-09 22:20:33 -0400

    v0.1.0: continue configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
     extend: {
